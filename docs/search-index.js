54eb96410d38750c70c3aa4cab017396f460b0a5
bestia.dev <info@bestia.dev>
2025-03-31 14:20:33 +0000

    update docs

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,4 +1,4 @@
-var searchIndex = new Map(JSON.parse('[["backup_for_zeljko_egui",{"t":"","n":[],"q":[],"i":"","f":"","D":"b","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]]'));
+var searchIndex = new Map(JSON.parse('[["backup_for_zeljko_egui",{"t":"","n":[],"q":[],"i":"","f":"","D":"b","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA=","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
-//{"start":39,"fragment_lengths":[130]}
+//{"start":39,"fragment_lengths":[137]}
